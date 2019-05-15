8323fb54e560c3c6cdd96700deb2f9f2ec1d7bc9
ozidrice <trolarddamien@gmail.com>
2019-05-15 23:04:29 +0200

    creating executable

---
+++ src/fr/upem/captcha/application/App.java
@@ -0,0 +1,16 @@
+package fr.upem.captcha.application;
+	
+import fr.upem.captcha.controller.AppController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		AppController.launch(primaryStage);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
--- src/fr/upem/captcha/application/Main.java
+++ src/fr/upem/captcha/application/Main.java
@@ -1,16 +1,7 @@
 package fr.upem.captcha.application;
-	
-import fr.upem.captcha.controller.AppController;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		AppController.launch(primaryStage);
-	}
-	
+public class Main {
 	public static void main(String[] args) {
-		launch(args);
+		App.main(args);
 	}
 }
